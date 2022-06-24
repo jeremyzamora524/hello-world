67bdd2c6962a3a0206db419bf163af22ab62e2de
jeremy zamora <jeremyzamora524@gmail.com>
2022-06-24 02:15:27 -0500

    Added UI

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,86 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble, MessageText, Time, SystemMessage } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
+  componentDidMount() {
+
+    const username = this.props.route.params.username;
+    const textColor = this.props.route.params.textColor;
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello ' + username,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Mr.Bot',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+        {
+          _id: 2,
+          text: username + ' has entered the chat',
+          createdAt: new Date(),
+          system: true,
+          color: textColor,
+        },
+      ],
+    })
+  }
+
+  renderBubble(props) {
+    const textColor = this.props.route.params.textColor;
+    const bubbleColor = this.props.route.params.bubbleColor;
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: bubbleColor,
+          }
+        }}
+        renderMessageText={(props) => {
+          return (
+            <MessageText
+              {...props}
+              textStyle={{
+                right: { color: textColor },
+              }}
+            />
+          );
+        }}
+        renderTime={(props) => {
+          return (
+            <Time
+              {...props}
+              timeTextStyle={{
+                right: {
+                  color: textColor,
+                },
+              }}
+            />
+          );
+        }}
+      />
+    )
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
 
   render() {
     let { username, bgColor } = this.props.route.params;
@@ -9,8 +88,18 @@ export default class Chat extends React.Component {
     this.props.navigation.setOptions({ title: username });
 
     return (
+
       <View style={[{ backgroundColor: bgColor }, styles.container]}>
-        <Text>UI is still Missing. Code me please!</Text>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+        }
       </View>
     );
   };
@@ -19,7 +108,5 @@ export default class Chat extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
 }); 
--- components/Start.js
+++ components/Start.js
@@ -1,37 +1,48 @@
 import React from 'react';
 import { View, TextInput, ImageBackground, TouchableOpacity, StyleSheet, Text, Pressable } from 'react-native';
 import BackgroundImage from '../img/Background.png';
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs(["EventEmitter.removeListener"]); //ignores automatically these warning logs. This method is depricated but I can't change it.
 
 const colorsCircle = {
   white: "white",
-  blue: "#79c2d0",
-  green: "#7dd87d",
+  blue: "#22559c",
   red: "#dc2f2f",
-  yellow: "#ffc93c",
+  orange: "#e4663a",
   purple: "#6643b5",
-  pink: "#f76b8a",
+  pink: "#ca4b7c",
 }
 
 export default class Start extends React.Component {
 
   colors = {
-    white: "white",
-    blue: "#79c2d0",
-    green: "#7dd87d",
+    white: "#ffffff",
+    blue: "#22559c",
+    green: "#2eb872",
     red: "#dc2f2f",
-    yellow: "#ffc93c",
+    orange: "#e4663a",
     purple: "#6643b5",
-    pink: "#f76b8a",
+    pink: "#ca4b7c",
+    black: "#000",
   }
 
   constructor(props) {
     super(props);
-    this.state = { username: '', bgColor: this.colors.white };
+    this.state = { username: '', bgColor: this.colors.white, textColor: this.colors.black, bubbleColor: this.colors.blue };
   }
 
   changeColor = (newColor) => {
     this.setState({ bgColor: newColor });
   };
+
+  changeTextColor = (newColor) => {
+    this.setState({ textColor: newColor });
+  }
+
+  changeBubbleColor = (newColor) => {
+    this.setState({ bubbleColor: newColor });
+  }
 
   render() {
     return (
@@ -46,54 +57,51 @@ export default class Start extends React.Component {
 
             <View style={styles.userBox}>
 
-              <Text style={styles.title2}>Choose your name:</Text>
-
-              <TextInput style={styles.title2}
+              <TextInput style={styles.input}
                 onChangeText={(username) => this.setState({ username })}
                 value={this.state.username}
-                placeholder='(eg. Steve)'
+                placeholder='Write your name...'
               />
 
             </View>
 
-            <Text style={styles.title3}>Choose a Background Color:</Text>
+            <View style={styles.colorBox}>
+              <Text style={styles.chooseColor}>Choose a Background Color:</Text>
+            </View>
 
             <View style={styles.colorScheme}>
 
               <TouchableOpacity
                 style={[{ backgroundColor: colorsCircle.white }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.white)}
+                onPress={() => { this.changeColor(this.colors.white), this.changeTextColor(this.colors.white), this.changeBubbleColor(this.colors.blue) }}
               />
 
               <TouchableOpacity
                 style={[{ backgroundColor: colorsCircle.blue }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.blue)}
+                onPress={() => { this.changeColor(this.colors.blue), this.changeTextColor(this.colors.black), this.changeBubbleColor(this.colors.green) }}
               />
-              <TouchableOpacity
-                style={[{ backgroundColor: colorsCircle.green }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.green)}
-              />
+
               <TouchableOpacity
                 style={[{ backgroundColor: colorsCircle.red }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.red)}
+                onPress={() => { this.changeColor(this.colors.red), this.changeTextColor(this.colors.white), this.changeBubbleColor(this.colors.blue) }}
               />
               <TouchableOpacity
-                style={[{ backgroundColor: colorsCircle.yellow }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.yellow)}
+                style={[{ backgroundColor: colorsCircle.orange }, styles.borderRadius]}
+                onPress={() => { this.changeColor(this.colors.orange), this.changeTextColor(this.colors.white), this.changeBubbleColor(this.colors.blue) }}
               />
               <TouchableOpacity
                 style={[{ backgroundColor: colorsCircle.purple }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.purple)}
+                onPress={() => { this.changeColor(this.colors.purple), this.changeTextColor(this.colors.black), this.changeBubbleColor(this.colors.green) }}
               />
               <TouchableOpacity
                 style={[{ backgroundColor: colorsCircle.pink }, styles.borderRadius]}
-                onPress={() => this.changeColor(this.colors.pink)}
+                onPress={() => { this.changeColor(this.colors.pink), this.changeTextColor(this.colors.white), this.changeBubbleColor(this.colors.blue) }}
               />
 
             </View>
 
             <Pressable style={styles.button}
-              onPress={() => this.props.navigation.navigate('Chat', { username: this.state.username, bgColor: this.state.bgColor })}
+              onPress={() => this.props.navigation.navigate('Chat', { username: this.state.username, bgColor: this.state.bgColor, textColor: this.state.textColor, bubbleColor: this.state.bubbleColor })}
             >
 
               <Text style={styles.buttonText}>Start Chatting</Text>
@@ -123,47 +131,61 @@ const styles = StyleSheet.create({
   },
 
   titleBox: {
-    height: "75%",
+    height: "50%",
     width: "88%",
     alignItems: "center",
-    paddingTop: 200,
+    paddingTop: 100,
   },
 
   title: {
     fontWeight: '600',
-    fontSize: 40,
+    fontSize: 45,
     color: 'white',
   },
 
   userChoices: {
     backgroundColor: "white",
-    height: "25%",
-    width: "100%",
+    height: "46%",
+    width: "88%",
     justifyContent: "space-around",
     alignItems: "center",
   },
 
   userBox: {
-    height: "40%",
-    width: "100%",
+    borderWidth: 2,
+    borderRadius: 1,
+    borderColor: "grey",
+    width: "88%",
+    height: 60,
+    paddingLeft: 20,
+    flexDirection: "row",
     alignItems: "center",
-    justifyContent: "center",
   },
 
-  title2: {
-    fontSize: 15,
+  input: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 0.5,
   },
 
-  title3: {
-    fontSize: 15,
-    paddingBottom: 15,
+  colorBox: {
+    marginRight: "auto",
+    paddingLeft: 15,
+    width: "88%",
+  },
+
+  chooseColor: {
+    fontSize: 16,
+    fontWeight: "300",
+    color: "#757083",
+    opacity: 1,
   },
 
   colorScheme: {
     flexDirection: "row",
     justifyContent: "space-between",
     width: "80%",
-    height: '33%',
   },
 
   borderRadius: {
@@ -175,16 +197,17 @@ const styles = StyleSheet.create({
 
   button: {
     width: "88%",
-    height: "28%",
+    height: 70,
     borderRadius: 8,
     backgroundColor: "#1D6085",
     alignItems: "center",
     justifyContent: "center",
-    paddingBottom: 5,
   },
 
   buttonText: {
-    color: 'white',
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "600",
   }
 
 });
